f209745c57cd6e68fb380f418fe304a51e6e437d
jonasdevzero <jonasdevzero@gmail.com>
2021-09-17 18:41:29 -0300

    test

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"
+const baseURL = process.env.NEXT_APP_API_URL || "http://localhost:5000"
 
 export default axios.create({ baseURL })
 
