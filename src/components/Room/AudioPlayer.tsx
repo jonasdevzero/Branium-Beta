77a46f4580e754637199cb25989a2540e1b2c0a5
jonasdevzero <jonasdevzero@gmail.com>
2021-12-24 09:15:47 -0300

    replacing audioplayer to room folder

--- src/components/Room/AudioPlayer.tsx
+++ src/components/Room/AudioPlayer.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useCallback } from "react"
+import { useState, useRef, useEffect } from "react"
 
 import {
     Container,
@@ -7,7 +7,7 @@ import {
     ProgressBar,
     Time,
     SpeedButton,
-} from "../styles/components/AudioPlayer"
+} from "../../styles/components/AudioPlayer"
 import {
     FaPause,
     FaPlay,
