227d3a61e0878cff108dca6b51fdbe1d2ffb4181
jonasdevzero <jonasdevzero@gmail.com>
2022-01-20 22:33:04 -0300

    starting group service

--- src/services/api/groupService.ts
+++ src/services/api/groupService.ts
@@ -1,12 +1,79 @@
-import { ContactMessage } from "../../types/user"
+import api from ".";
+import { GroupMessage } from "../../types/user";
 
-const groupService = {
-    async getMessages() {
-        return [] as ContactMessage[]
-    },
-
-    async viewMessages() {
-    }
+interface CreateGroupI {
+  name: string;
+  description: string;
+  picture: File;
+  members?: string[];
 }
 
-export default groupService
+const groupService = {
+  show(id: string) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { data } = await api.get(`/group/${id}`);
+
+        resolve(data.group);
+      } catch (error: any) {
+        reject(error.response?.data.message);
+      }
+    });
+  },
+
+  async create(data: CreateGroupI) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await api.post("/group", data);
+
+        resolve(response.data.group);
+      } catch (error: any) {
+        reject(error.response?.data.message);
+      }
+    });
+  },
+
+  async update() {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (error: any) {
+        reject(error.response?.data.message);
+      }
+    });
+  },
+
+  async update_picture() {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (error: any) {
+        reject(error.response?.data.message);
+      }
+    });
+  },
+
+  async leave() {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (error: any) {
+        reject(error.response?.data.message);
+      }
+    });
+  },
+
+  async delete() {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (error: any) {
+        reject(error.response?.data.message);
+      }
+    });
+  },
+
+  async getMessages() {
+    return [] as GroupMessage[];
+  },
+
+  async viewMessages() {},
+};
+
+export default groupService;
