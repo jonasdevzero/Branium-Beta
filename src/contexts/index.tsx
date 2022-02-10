74082728230b9d20f6729ae9f196f1714d3f1145
jonasdevzero <jonasdevzero@gmail.com>
2022-02-10 08:26:21 -0300

    config redux-persist

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,19 +1,22 @@
 import { Provider as ReduxProvider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
 import { WarnProvider } from "./WarnContext"
 import { AuthProvider } from "./AuthContext"
 import { CallProvider } from "./CallContext"
-import store from "../store"
+import store, { persistor } from "../store"
 
 export default function Contexts({ children }: { children: React.ReactChild }) {
   return (
     <ReduxProvider store={store}>
-      <WarnProvider>
-        <AuthProvider>
-          <CallProvider>
-            {children}
-          </CallProvider>
-        </AuthProvider>
-      </WarnProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <WarnProvider>
+          <AuthProvider>
+            <CallProvider>
+              {children}
+            </CallProvider>
+          </AuthProvider>
+        </WarnProvider>
+      </PersistGate>
     </ReduxProvider>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,22 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'branium',
+  storage,
+  whitelist: ['settings'],
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
